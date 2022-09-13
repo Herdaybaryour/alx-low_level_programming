0330253b02a7679958da66e228286361aac1f003
Herdaybaryour <bayo450@gmail.com>
2022-09-13 07:45:18 -0700

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * islower - function to print lower alphabet
+ *
+ * Return:0 always
+ */
+int _islower(int c)
+{
+	if (c >= 'a'; && c >= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
