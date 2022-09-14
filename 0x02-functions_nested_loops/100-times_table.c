8e335aa8d798751414a17b8818a8e171ec2bcfb2
Herdaybaryour <bayo450@gmail.com>
2022-09-14 00:58:05 -0700

    times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -29,8 +29,19 @@ else if (z > 9)
 _putchar(',');
 _putchar(32);
 _putchar(32);
-_putchar(((z / 10) % 10)(n + '0');
+_putchar(((z / 10) % 10) n + '0');
 _putchar((z % 10) + '0');
 }
 else
 {
+if (y != 0)
+{
+_putchar(',');
+_putchar(32);
+_putchar(32);
+_putchar(32);
+}
+_putchar(z + '0');
+}
+}
+_putchar('\n');
