992bbf9cf0b4219bdca9b6125237a6a3d6ded0b0
Herdaybaryour <bayo450@gmail.com>
2022-09-22 12:57:32 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,7 +7,7 @@
  * @n: numbers of chars to be copied
  * Return: dest
  */
-char *_strncat(char *destm char *src, int n)
+char *_strncat (char *dest, char *src, int n)
 int i = 0, j = 0;
 while (*(dest + i) != '\0')
 i++;
