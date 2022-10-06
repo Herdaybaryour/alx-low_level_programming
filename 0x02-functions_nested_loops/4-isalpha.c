3822946caac22edd13d6e00d1261cc4299811c1e
Herdaybaryour <bayo450@gmail.com>
2022-10-05 17:26:53 -0700

    ll

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,24 +1,17 @@
 #include "main.h"
-
 /**
-  *_isalpha - Entry point
-  * @c: the integer value it receives
-  * Description: checks if alphabet
-  * Return: 1 if true. 0 if false
-  */
-
-int _isalpha(int c)
+ * _islower - function to print lower alphabet
+ *
+ *@c:for the argument function
+ *
+ * Return:0 always
+ */
+int _islower(int c)
 {
-if (c >= 'a' && c <= 'z')
-{
-return (1);
-}
-else if (c >= 'A' && c <= 'Z')
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
 }
--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,14 +1,14 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-  *create_array - creates an array of chars and initializes it with
-  *a specific char.
-  *@size: size of array.
-  *@c: char to initialize array with.
-  *
-  *Return: NULL if it fails or
-  *Pointer to array.
-  */
+ * create_array - creates an array of chars and initializes it with
+ * a specific char.
+ * @size: size of array.
+ * @c: char to initialize array with.
+ *
+ * Return: NULL if it fails or
+ * Pointer to array.
+ */
 char *create_array(unsigned int size, char c)
 {
 char *arr;
